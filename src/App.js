ba429c69a19129386280642e06a1ec80d3708347
Vanroy Julien <julienvanroy@yahoo.fr>
2019-05-01 14:31:27 +0200

    Add Scss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
-import './App.css';
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
