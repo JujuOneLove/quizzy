d77ca3df4516a4ad3cf2c12b852d5ad0c133849b
Vanroy Julien <julienvanroy@yahoo.fr>
2019-05-06 22:23:18 +0200

    Footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer"
 import Home from "./pages/Home";
 import Personnes from "./pages/Personnes";
 import Quotations from "./pages/Quotations";
@@ -10,19 +11,18 @@ function App() {
   return (
       <BrowserRouter>
         <div>
-          <header>
+          <header className="header">
             <Navbar/>
           </header>
-          <Switch>
-            <Route exact={true} path='/' component={Home}/>
-            <Route exact={true} path='/home' component={Home}/>
-            <Route exact={true} path='/persons' component={Personnes}/>
-            <Route exact={true} path='/quotes' component={Quotations}/>
-            <Route path="*" component={Error} />
-          </Switch>
-          <footer>
-            <span>© 2015 IUT de Lens - Rue de l’université -SP 16 - 62307 LENS Cedex - FRANCE - Tel : +33 (0)3 21 79 32 32</span>
-          </footer>
+          <main role="main">
+            <Switch>
+              <Route exact={true} path='/' component={Home}/>
+              <Route exact={true} path='/persons' component={Personnes}/>
+              <Route exact={true} path='/quotes' component={Quotations}/>
+              <Route path="*" component={Error} />
+            </Switch>
+          </main>
+          <Footer/>
         </div>
       </BrowserRouter>
   );
