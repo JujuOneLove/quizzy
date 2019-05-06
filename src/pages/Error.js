32f459ee497aef51301742b7cf39cdaabbfb88e7
Vanroy Julien <julienvanroy@yahoo.fr>
2019-05-06 23:25:41 +0200

    Error 404

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -11,7 +11,7 @@ export default class Error extends Component {
                 <div className="bubble"></div>
                 <div className="content">
                     <h1>404</h1>
-                    <p>It looks like you're lost...<br/>That's a trouble?</p>
+                    <p>On dirait que tu es perdu ... C'est un problème?</p>
                     <Link className="btn" to="/">Page d'accueil</Link>
                 </div>
             </div>
