caf9fa9c170fa01998e2e91bcc4b488007ba7dc6
Vanroy Julien <julienvanroy@yahoo.fr>
2019-05-06 18:37:47 +0200

    Logo / Favicon

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class Form extends Component {
+    render() {
+        return (
+            <footer>
+                <span>© Quizzy  2019 - All rights reserved - Mentions légales  - Plan du site</span>
+            </footer>
+        )
+    }
+}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,25 @@
 import React, {Component} from 'react';
 import {Link} from "react-router-dom";
-
+import logo from "../assets/svg/logo.svg"
 
 export default class Navbar extends Component {
     render() {
         return (
-            <nav className="navbar">
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/persons">Personnes</Link>
-                    </li>
-                    <li>
-                        <Link to="/quotes">Quotations</Link>
-                    </li>
-                    <li>
-                        <Link to="/error">Test Error</Link>
-                    </li>
-                </ul>
+            <nav className="nav">
+                <div className="container">
+                    <Link to="/"><img className="logo" src={logo} alt="Quizzy - Site Officiel des Quiz"/></Link>
+                    <ul>
+                        <li>
+                            <Link to="/persons">Personnes</Link>
+                        </li>
+                        <li>
+                            <Link to="/quotes">Quotations</Link>
+                        </li>
+                        <li>
+                            <Link to="/error">Test Error</Link>
+                        </li>
+                    </ul>
+                </div>
             </nav>
         );
     }
