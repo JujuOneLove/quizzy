0e38f3b135fbfb38c2035ce73bc82ce1fab79676
Vanroy Julien <julienvanroy@yahoo.fr>
2019-05-08 15:55:58 +0200

    Correction Search Bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,17 +4,20 @@ import logo from "../assets/svg/logo.svg"
 import user from "../assets/svg/user.svg"
 import search from "../assets/svg/search.svg"
 import axios from "axios";
-import Search from "./Search";
 export default class Navbar extends Component {
     constructor(props) {
         super(props);
         this.state = {
             quizzes: [],
-            search :""
+            search :"",
+            btn: false
         };
     }
     onChange(event) {
         this.setState({search: event.target.value});
+    }
+    onClick() {
+        this.setState({btn: !this.state.btn});
     }
     async componentDidMount() {
         const quizzes = (await axios.get('http://localhost:8081/quizzes')).data;
@@ -24,8 +27,8 @@ export default class Navbar extends Component {
     }
 
     render() {
-        if(this.state.search !== ""){
-            let searchTab = this.state.quizzes.filter(quizz => ((quizz.name.indexOf(this.state.search)) !== -1) );
+            let searchFilter = this.state.quizzes.filter(quizz => ((quizz.name.indexOf(this.state.search)) !== -1) );
+            let searchTab = searchFilter.map(quiz => <li key={quiz._id}><Link to={"/jouer/"+quiz._id}>{quiz.name}</Link></li>);
             return (
                 <nav className="nav">
                     <div className="container">
@@ -34,9 +37,14 @@ export default class Navbar extends Component {
                             <li>
                                 <div className="search">
                                     <input type="text" placeholder="Chercher un quiz" value={this.state.search} onChange={this.onChange.bind(this)}/>
-                                    <button><img src={search} alt="Chercher un quiz"/></button>
-                                    <div id="filtre">
-                                        <Search quizzes={searchTab}/>
+                                    <button onClick={this.onClick.bind(this)}><img src={search} alt="Chercher un quiz"/></button>
+                                    <div id="filtre" className={this.state.search ? "active" : this.state.btn ? "mobile" : ""}>
+                                        <div className="filter container">
+                                            <ul>
+                                                <li><input type="text" placeholder="Chercher un quiz" value={this.state.search} onChange={this.onChange.bind(this)}/></li>
+                                                {this.state.search ? searchTab : null}
+                                            </ul>
+                                        </div>
                                     </div>
                                 </div>
                             </li>
@@ -47,28 +55,7 @@ export default class Navbar extends Component {
                     </div>
                 </nav>
             );
-        } else {
-            return (
-                <nav className="nav">
-                    <div className="container">
-                        <Link to="/"><img className="logo" src={logo} alt="Quizzy - Site Officiel des Quiz"/></Link>
-                        <ul>
-                            <li>
-                                <div className="search">
-                                    <input type="text" placeholder="Chercher un quiz" value={this.state.search} onChange={this.onChange.bind(this)}/>
-                                    <button><img src={search} alt="Chercher un quiz"/></button>
-                                </div>
-                            </li>
-                            <li>
-                                <Link to="/admin"><img src={user} alt="Connexion"/></Link>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
-            );
         }
-
-    }
 }
 
 
