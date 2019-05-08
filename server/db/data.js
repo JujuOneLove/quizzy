4293008523ca95d429be1de669225196f7f9d3eb
Anthony Hacart <anthony.hacart@gmail.com>
2019-05-08 21:52:15 +0200

    fin creer quiz, manque redirection vers accueil

--- server/db/data.js
+++ server/db/data.js
@@ -3,25 +3,25 @@ db.users.remove({});
 db.quizzes.remove({});
 db.users.insert(
     {
-        "user": "admin",
+        "name": "admin",
         "password": "admin"
     });
 db.users.insert(
     {
-        "user": "antho",
+        "name": "antho",
         "password": "antho"
     });
 db.users.insert(
     {
-        "user": "flo",
+        "name": "flo",
         "password": "flo"
     });
 db.users.insert(
     {
-        "user": "jul",
+        "name": "jul",
         "password": "jul"
     });
-var user2 = db.users.findOne({"user" : "antho"});
+var user2 = db.users.findOne({ "name": "antho" });
 
 var quiz1 = db.quizzes.insert({
     "name": "La France",
@@ -33,22 +33,22 @@ var quiz1 = db.quizzes.insert({
             "question": "À l’origine, le Louvre était une prison.",
             "point": 1,
             "answers": [
-                {"valid": false, "answerText": "Vrai"},
-                {"valid": true, "answerText": "Faux"}]
+                { "valid": false, "answerText": "Vrai" },
+                { "valid": true, "answerText": "Faux" }]
         },
         {
             "question": "Le général de Gaulle a été président de la France avant et après la Seconde Guerre mondiale.",
             "point": 1,
             "answers": [
-                {"valid": false, "answerText": "Vrai"},
-                {"valid": true, "answerText": "Faux"}]
+                { "valid": false, "answerText": "Vrai" },
+                { "valid": true, "answerText": "Faux" }]
         },
         {
             "question": "« La drôle de guerre » est la période qui s’est écoulée entre les deux Grandes Guerres.",
             "point": 1,
             "answers": [
-                {"valid": false, "answerText": "Vrai"},
-                {"valid": true, "answerText": "Faux"}]
+                { "valid": false, "answerText": "Vrai" },
+                { "valid": true, "answerText": "Faux" }]
         },
     ]
 });
@@ -63,22 +63,22 @@ var quiz2 = db.quizzes.insert({
             "question": "Avec 13 buts, Just Fontaine détient le record du plus grand nombre de buts marqués au cours d’un seul tournoi.",
             "point": 1,
             "answers": [
-                {"valid": true, "answerText": "Vrai"},
-                {"valid": false, "answerText": "Faux"}]
+                { "valid": true, "answerText": "Vrai" },
+                { "valid": false, "answerText": "Faux" }]
         },
         {
             "question": "En 2006, Lionel Messi a aidé l’Argentine à remporter la Coupe du monde de football.",
             "point": 1,
             "answers": [
-                {"valid": false, "answerText": "Vrai"},
-                {"valid": true, "answerText": "Faux"}]
+                { "valid": false, "answerText": "Vrai" },
+                { "valid": true, "answerText": "Faux" }]
         },
         {
             "question": "Les médaillés d’or aux Jeux olympiques ont de fortes chances de remporter la Coupe du monde deux années plus tard.",
             "point": 1,
             "answers": [
-                {"valid": false, "answerText": "Vrai"},
-                {"valid": true, "answerText": "Faux"}]
+                { "valid": false, "answerText": "Vrai" },
+                { "valid": true, "answerText": "Faux" }]
         },
     ]
 });
--- server/router.js
+++ server/router.js
@@ -19,7 +19,7 @@ router
                 res.json({
                     error: "Bad request"
                 });
-            }else{
+            } else {
                 res.json(quizzes);
                 res.status(200);
             }
@@ -32,37 +32,36 @@ router
                 res.json({
                     error: "Bad request"
                 });
-            }else{
+            } else {
                 res.json(quizzes);
                 res.status(200);
             }
         });
     })
-    .post('/upload',
-        (req, res) => {
-            req.files.picture.mv(__dirname + '/resources/pictures/' + req.files.picture.name,
-                (err) => {
-                    if (err)
-                        return res.status(500).send(err);
-                    res.json({ok: "ok"});
-                }
-            );
-        })
-    .post("/quizzes/new", (req, res)=>{
+    .post("/quizzes/new", (req, res) => {
         const quiz = req.body;
+        console.log(quiz);
+        req.files.picture.mv(__dirname + '/resources/pictures/' + req.files.picture.name,
+            (err) => {
+                if (err)
+                    return res.status(500).send(err);
+            }
+        );
+
         Quizzes.create({
             name: quiz.name,
-            logo: 'images/uploads/'+req.files.picture.filename,
-            createdBy: quiz.createdBy,
-            keywords: quiz.keywords,
-            questionsAndAnswers: quiz.questionsAndAnswers
+            logo: req.files.picture.name,
+            createdBy: JSON.parse(quiz.createdBy),
+            keywords: JSON.parse(quiz.keywords),
+            questionsAndAnswers: JSON.parse(quiz.questions)
         }, function (err, quizBdd) {
             if (err) {
                 res.status(400);
+                console.log('err', err)
                 res.json({
                     error: "Bad request"
                 });
-            }else{
+            } else {
                 res.json(quizBdd);
                 res.status(200);
             }
